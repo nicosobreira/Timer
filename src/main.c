eabf099b67d0e06ef8953b7d63a7193db2df1be5
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-11-09 17:57:18 -0300

    toInt function receives a pointer

--- src/main.c
+++ src/main.c
@@ -2,14 +2,10 @@
 
 #define LEN(ARR) ((sizeof(ARR) / sizeof(ARR[0])))
 
-int toInt(char *str) {
-  int num;
-  sscanf(str, "%04d", &num);
-  return num;
-}
+void toInt(char *str, int *num) { sscanf(str, "%04d", num); }
 
 int main(int argc, char *argv[]) {
-  unsigned int time[3] = {0, 0, 0}; // Stores hours, minutes and seconds
+  int time[3] = {0, 0, 0}; // Stores hours, minutes and seconds
   unsigned int total_seconds = 0;
   switch (argc - 1) {
   case 0:
@@ -26,18 +22,18 @@ int main(int argc, char *argv[]) {
     break;
     ;
   case 1:
-    time[2] = toInt(argv[1]);
+    toInt(argv[1], &time[2]);
     break;
     ;
   case 2:
-    time[1] = toInt(argv[1]) * 60;
-    time[2] = toInt(argv[2]);
+    toInt(argv[1], &time[1]);
+    toInt(argv[2], &time[2]);
     break;
     ;
   case 3:
-    time[0] = toInt(argv[1]) * 3600;
-    time[1] = toInt(argv[2]) * 60;
-    time[2] = toInt(argv[3]);
+    toInt(argv[1], &time[0]);
+    toInt(argv[2], &time[1]);
+    toInt(argv[3], &time[2]);
     break;
     ;
   default:
@@ -45,6 +41,7 @@ int main(int argc, char *argv[]) {
     return -1;
   }
   for (int i = 0; i < LEN(time); i++) {
+    printf("%i\n", time[i]);
     total_seconds += time[i];
   }
   printf("%o\n", total_seconds);
