41a8fad584f5679a26ee0ae52c7ef7cfbca9fac9
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-11-11 19:19:05 -0300

    Add pointer to `date`

--- src/main.c
+++ src/main.c
@@ -6,12 +6,13 @@ int main(int, char *[]);
 
 int timer(int argc, char *argv[]) {
   Date date;
+  Date *pdate = &date;
 
-  setDate(argc - 1, argv, &date);
+  setDate(argc - 1, argv, pdate);
 
   int total_seconds = (date.hour * 3600) + (date.min * 60) + date.sec;
-  printDate(&date, ":");
-  loop(total_seconds, &date);
+  printDate(pdate, ":");
+  loop(total_seconds, pdate);
   return 0;
 }
 
