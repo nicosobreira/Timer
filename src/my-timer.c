03e33908a55f69fd4817f68385cd610b9bf54bad
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-11-11 09:40:40 -0300

    Break down functions to files

--- src/my-timer.c
+++ src/my-timer.c
@@ -2,3 +2,21 @@
 #include <stdio.h>
 
 void toInt(char *str, int *num) { sscanf(str, "%04d", num); }
+
+void askTime(Time *time) {
+  printf("\tDigit the following values:");
+  printf("\nHours: ");
+  scanf("%i", &time->sec);
+
+  printf("Minutes: ");
+  scanf("%i", &time->min);
+
+  printf("Seconds: ");
+  scanf("%i", &time->hour);
+}
+
+void printTime(Time *time, char *sep) {
+  printf("%i%s", time->hour, sep);
+  printf("%i%s", time->min, sep);
+  printf("%i\n", time->sec);
+}
--- src/my-timer.h
+++ src/my-timer.h
@@ -1,6 +1,16 @@
 #ifndef MY_TIMER_H
 #define MY_TIMER_H
 
+typedef struct Time {
+  int sec;
+  int min;
+  int hour;
+} Time;
+
 void toInt(char *, int *);
+
+void askTime(Time *);
+
+void printTime(Time *, char *);
 
 #endif
