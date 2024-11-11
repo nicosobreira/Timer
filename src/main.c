ab9b585eb2af7cccc36321535dce39cdcadee6ed
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-11-11 13:23:27 -0300

    Rewrite

--- src/main.c
+++ src/main.c
@@ -3,51 +3,26 @@
 #include <time.h>
 #include <unistd.h>
 
-/*#define LEN(ARR) ((sizeof(ARR) / sizeof(ARR[0])))*/
-
-int main(int argc, char *argv[]) {
-  Date date;
-  time_t initial = 0, passed = 0;
-  int total_seconds;
-
-  switch (argc - 1) {
-  case 0:
-    /*askDate(&date);*/
-    date.hour = 0;
-    date.min = 0;
-    date.sec = 2;
-    break;
-    ;
-  case 1:
-    toInt(argv[1], &date.sec);
-    break;
-    ;
-  case 2:
-    toInt(argv[1], &date.min);
-    toInt(argv[2], &date.sec);
-    break;
-    ;
-  case 3:
-    toInt(argv[1], &date.hour);
-    toInt(argv[2], &date.min);
-    toInt(argv[3], &date.sec);
-    break;
-    ;
-  default:
-    printf("Digit nothing or three number at maximum\n");
-    return -1;
-  }
-
-  total_seconds = (date.hour * 3600) + (date.min * 60) + date.sec;
-  printDate(&date, ":");
+void loop(int total_seconds, Date *date) {
+  time_t initial, passed;
   initial = time(NULL);
   while (1) {
+    passed = time(NULL) - initial;
+    printDate(date, ":");
     if (passed >= total_seconds) {
       printf("Acabou!\n");
       break;
     }
-    /*usleep(5);*/
-    passed = time(NULL) - initial;
+    usleep(3);
   }
+}
+
+int main(int argc, char *argv[]) {
+  Date date;
+
+  setDate(argc - 1, argv, &date);
+
+  int total_seconds = (date.hour * 3600) + (date.min * 60) + date.sec;
+  loop(total_seconds, &date);
   return 0;
 }
