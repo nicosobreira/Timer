d72f580557575586484a958c6542f4b069f5fbb1
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-11-11 10:36:57 -0300

    Working timer!!!

--- src/main.c
+++ src/main.c
@@ -1,30 +1,36 @@
 #include "my-timer.h"
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 
 /*#define LEN(ARR) ((sizeof(ARR) / sizeof(ARR[0])))*/
 
 int main(int argc, char *argv[]) {
-  Time time;
-  int total_seconds = 0;
+  Date date;
+  time_t initial = 0, passed = 0;
+  int total_seconds;
 
   switch (argc - 1) {
   case 0:
-    askTime(&time);
+    /*askDate(&date);*/
+    date.hour = 0;
+    date.min = 0;
+    date.sec = 2;
     break;
     ;
   case 1:
-    toInt(argv[1], &time.sec);
+    toInt(argv[1], &date.sec);
     break;
     ;
   case 2:
-    toInt(argv[1], &time.min);
-    toInt(argv[2], &time.sec);
+    toInt(argv[1], &date.min);
+    toInt(argv[2], &date.sec);
     break;
     ;
   case 3:
-    toInt(argv[1], &time.hour);
-    toInt(argv[2], &time.min);
-    toInt(argv[3], &time.sec);
+    toInt(argv[1], &date.hour);
+    toInt(argv[2], &date.min);
+    toInt(argv[3], &date.sec);
     break;
     ;
   default:
@@ -32,8 +38,16 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  total_seconds = (time.hour * 3600) + (time.min * 60) + time.sec;
-  printTime(&time, ":");
-  printf("%i\n", total_seconds);
+  total_seconds = (date.hour * 3600) + (date.min * 60) + date.sec;
+  printDate(&date, ":");
+  initial = time(NULL);
+  while (1) {
+    if (passed >= total_seconds) {
+      printf("Acabou!\n");
+      break;
+    }
+    /*usleep(5);*/
+    passed = time(NULL) - initial;
+  }
   return 0;
 }
